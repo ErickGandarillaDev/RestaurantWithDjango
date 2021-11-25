ac3e50518f368cdf6e13690d27efd31e583985ba
SpecTrumd3v <17041298@itdurango.edu.mx>
2021-11-25 18:26:11 +0000

    Restaurant test

--- infores/tests.py
+++ infores/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class RestaurantModelTests(TestCase):
+	def test_restaurant_name(self):
+		new_restaurant = Job(name='Enroute Rest')
+		print(new_restaurant)
+		self.assertIs(str(new_restaurant), 'Enroute Rest')
