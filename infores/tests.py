00674bc6bece817bc62cbbeac9effc86fbdb225d
SpecTrumd3v <17041298@itdurango.edu.mx>
2021-11-25 18:28:04 +0000

    Restaurant test

--- infores/tests.py
+++ infores/tests.py
@@ -3,6 +3,6 @@ from .models import *
 
 class RestaurantModelTests(TestCase):
 	def test_restaurant_name(self):
-		new_restaurant = Job(name='Enroute Rest')
+		new_restaurant = Restaurant(name='Enroute Rest')
 		print(new_restaurant)
 		self.assertIs(str(new_restaurant), 'Enroute Rest')
